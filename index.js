4519bd18175ecfb42b542a6cc75ba68f6c50dfc3
Richard Vernon Dyson <dyson77z@gmail.com>
2020-01-16 02:15:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,12 @@ function sayHiToGrandma(string) {
 
   return "Are you eating enough?"
 }
+it('returns "I love you, too." if `string` is "I love you, Grandma."`', () => {
+      expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+    })
+
+    it('returns "Are you eating enough?" otherwise', () => {
+      expect(sayHiToGrandma('Hello')).toEqual("Are you eating enough?")
+    })
+  })
+})
