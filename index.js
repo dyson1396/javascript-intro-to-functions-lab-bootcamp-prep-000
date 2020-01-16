3852439b1c06bf44ca780c9d2bc72343ae2004d3
Richard Vernon Dyson <dyson77z@gmail.com>
2020-01-16 02:13:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function shout(string) {
   return string.toUpperCase()
 }
-function sayHiTo(firstName) {
-  console.log(`Hi, ${firstName}!`)
+function logWhisper(string) {
+  console.log(string.toLowerCase())
 }
- 
-sayHiTo('Grandma')
- return string.toLowerCase()
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+}
